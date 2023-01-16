892ef68d5f8e5eeb55b4764ef927e516372c636e
SimonHNoerkjaer <118724499+SimonHNoerkjaer@users.noreply.github.com>
2023-01-16 16:27:24 +0100

    asd

--- External_Functions/MyExternalFunctions.py
+++ External_Functions/MyExternalFunctions.py
@@ -290,7 +290,7 @@ def binning(x, xrange, Nbins, remove_empty=True):
 
 
 #Plotting histogram:
-def easy_hist(x, xrange, Nbins, Figsize=(10, 7), title= None):
+def easy_hist(x, xrange, Nbins, Figsize=(10, 7), title= None, x_label= 'x'):
     '''Function for plotting a histogram
     
        x: data to be binned
@@ -301,7 +301,7 @@ def easy_hist(x, xrange, Nbins, Figsize=(10, 7), title= None):
     fig, ax = plt.subplots(figsize=Figsize)
     ax.hist(x, bins=Nbins ,range=xrange, histtype='stepfilled', color='lightgreen', edgecolor='grey', linewidth=1.2)
     ax.errorbar(bin_centers, counts, yerr=np.sqrt(counts), fmt='o',mec='k',mfc='g', capsize=1, ecolor='g')
-    ax.set(xlabel='x', ylabel=f'Counts / {binwidth:.2f}', title=title)
+    ax.set(xlabel=x_label, ylabel=f'Counts / {binwidth:.2f}', title=title)
     plt.show()
     return fig, ax , counts
     
